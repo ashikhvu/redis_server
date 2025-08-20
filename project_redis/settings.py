848b957996af2cd7b17348675530723d2dd60503
ashikhvu <vuashikh16@gmail.com>
2025-08-20 22:44:00 +0530

    2nd commit

--- project_redis/settings.py
+++ project_redis/settings.py
@@ -87,11 +87,16 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default="postgres://user:pass@host:port/dbname")
 }
 
 
